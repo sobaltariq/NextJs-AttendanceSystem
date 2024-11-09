2386c640c8f77f5c62dba7032ce442bf0753702a
SobalWork <sobalwork@gmail.com>
2024-11-09 23:39:40 +0500

    adding fonts

---
+++ src/app/AllDataWrapper.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import React from "react";
+
+const AllDataWrapper: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <>
+      <Header />
+      <div className="width-container">{children}</div>
+      <Footer />
+    </>
+  );
+};
+
+export default AllDataWrapper;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
+import AllDataWrapper from "./AllDataWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,9 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        {children}
-        <Footer />
+        <AllDataWrapper>{children}</AllDataWrapper>
       </body>
     </html>
   );
