5379a1379d76e84e768c0a5f2edbc86e21572727
SobalWork <sobalwork@gmail.com>
2025-03-26 03:00:45 +0500

    working on group chat

--- src/app/team-interaction-center/page.tsx
+++ src/app/team-interaction-center/page.tsx
@@ -5,9 +5,11 @@ import UsersList from "@/components/interaction/UsersList";
 import AppModal from "@/components/modal/AppModal";
 import CreateGroupChatModal from "@/components/modal/CreateGroupChatModal";
 import { useMessageModal } from "@/components/modal/providers/MessageModalProvider";
+import { RootState } from "@/redux/store";
 import { UsersListInterface } from "@/types/api";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 // get - all - user;
 const TeamInteractionCenter: React.FC = () => {
   const [users, setUsers] = useState<UsersListInterface[]>([]);
@@ -21,6 +23,9 @@ const TeamInteractionCenter: React.FC = () => {
 
   // to show any message popup
   const { showMessageModal } = useMessageModal();
+
+  const { userRole } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
 
   const fetchUsers = async (signal: AbortSignal) => {
     const loggedInUser = localStorage.getItem("loggedInUser");
@@ -56,6 +61,40 @@ const TeamInteractionCenter: React.FC = () => {
       controller.abort();
     };
   }, []);
+  const fetchGroups = async (signal: AbortSignal) => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    try {
+      if (!loggedInUser) {
+        await showMessageModal("error", "Something Went Wrong");
+        return;
+      }
+      const response = await MyApi.get(`/chats/get-groups`);
+      const { success, totalUsers, users } = response.data;
+      console.log("0000", response.data);
+
+      if (success) {
+        // setUsers(users);
+      }
+    } catch (err: any) {
+      const errorMessage =
+        err.response?.data?.error ||
+        err.response?.data?.message ||
+        "Something went wrong";
+      showMessageModal("error", errorMessage, 5000);
+      console.log("errorMessage", err);
+    }
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetchGroups(signal);
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   useEffect(() => {
     const chatIdFromUrl = searchParams.get("chatId");
@@ -74,12 +113,14 @@ const TeamInteractionCenter: React.FC = () => {
       <div className="width-container">
         <section className="interaction-container">
           <h2>Team Interaction Center</h2>
-          <button
-            className="btn-primary"
-            onClick={() => setCreateGroupChat(true)}
-          >
-            Create Group Chat
-          </button>
+          {userRole == "admin" && (
+            <button
+              className="btn-primary"
+              onClick={() => setCreateGroupChat(true)}
+            >
+              Create Group Chat
+            </button>
+          )}
           <div
             className="interaction-wrapper"
             data-chat={currentChatId == null}
--- src/components/modal/CreateGroupChatModal.tsx
+++ src/components/modal/CreateGroupChatModal.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { SubmitButton } from "../buttons/CustomButtons";
 import { UsersListInterface } from "@/types/api";
+
+import { RxCross2 } from "react-icons/rx";
+import { useSocket } from "@/context/SocketContext";
+import { useMessageModal } from "./providers/MessageModalProvider";
 
 interface CreateGroupChatInterface {
   users: UsersListInterface[];
@@ -12,11 +16,16 @@ const CreateGroupChatModal: React.FC<CreateGroupChatInterface> = ({
   onClose,
 }) => {
   const [groupName, setGroupName] = useState<string>("");
-  const [participants, setParticipants] = useState<string[]>([]);
+  const [participants, setParticipants] = useState<
+    { id: string; name: string }[]
+  >([]);
   const [currentParticipant, setCurrentParticipant] = useState<string>("");
   const [loggedInUserId, setLoggedInUserId] = useState<string | null>(null);
 
   const [toggleUsers, setToggleUsers] = useState<boolean>(false);
+
+  const { emitEvent, onEvent, offEvent } = useSocket();
+  const { showMessageModal } = useMessageModal();
 
   useEffect(() => {
     // Fetch logged-in user ID from localStorage
@@ -29,7 +38,7 @@ const CreateGroupChatModal: React.FC<CreateGroupChatInterface> = ({
         console.error("Error parsing loggedInUser:", error);
       }
     }
-  }, [loggedInUserId]);
+  }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -37,39 +46,67 @@ const CreateGroupChatModal: React.FC<CreateGroupChatInterface> = ({
       setGroupName(value);
     } else if (name === "participants") {
       setCurrentParticipant(value);
+      setToggleUsers(true);
     }
   };
 
-  const handleAddParticipant = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    console.log("====================================");
-    console.log(toggleUsers);
-    console.log("====================================");
-    if (e.key === "Enter" && currentParticipant.trim() !== "") {
-      e.preventDefault();
-      setParticipants((prev) => [...prev, currentParticipant.trim()]);
-      setCurrentParticipant(""); // Clear input field
-    }
-  };
+  const handleAddParticipant = useCallback(
+    (userId: string, name: string) => {
+      if (!name.trim() || participants.some((p) => p.id === userId)) return;
+      setToggleUsers(false);
+      setParticipants((prev) => [...prev, { id: userId, name }]);
+      setCurrentParticipant("");
+    },
+    [participants]
+  );
+
+  const handleRemoveParticipant = useCallback((userId: string) => {
+    setParticipants((prev) => prev.filter((p) => p.id !== userId));
+  }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    const chatType = "group";
+    const participantIds = participants.map((p) => p.id);
+
+    console.log("ok", { loggedInUserId, chatType, groupName, participants });
+
     if (groupName.trim() === "" || participants.length === 0) return;
 
-    console.log("Creating group chat with:", { groupName, participants });
+    console.log("Creating group chat with:", {
+      groupName,
+      chatType,
+      userId: loggedInUserId,
+      participantIds,
+    });
 
-    // Call API here to create group chat
+    emitEvent("joinRoom", {
+      userId: loggedInUserId,
+      chatType,
+      groupName,
+      participantIds,
+    });
+    onEvent<{
+      chatId: string;
+      chatType: string;
+      message: string;
+      success: boolean;
+    }>("roomJoined", (data) => {
+      console.log("Private Chat Event:", data);
+      if (!data.success) {
+        // onUserSelect(data.chatId); // Update URL
+        // setActiveUserId(userId);
+        showMessageModal("error", data.message, 5000);
+      }
 
-    onClose();
+      onClose();
+      showMessageModal("success", data.message, 5000);
+
+      // Update the URL without refreshing the page
+      // router.push(`/team-interaction-center?chatId=${data.chatId}`);
+    });
   };
-
-  // Grouping users by role
-  const groupedUsers = Object.groupBy(users, (user) => user.role);
-
-  // Combine admins first, then other users
-  const sortedUsers = [
-    ...(groupedUsers.admin || []),
-    ...(groupedUsers.user || []),
-  ];
 
   return (
     <div className="create-group-modal">
@@ -86,35 +123,74 @@ const CreateGroupChatModal: React.FC<CreateGroupChatInterface> = ({
           type="text"
           name="participants"
           id="participants"
-          placeholder="Enter Member & Press Enter"
+          placeholder="Search Member"
           value={currentParticipant}
           onChange={handleInputChange}
-          onKeyDown={handleAddParticipant}
         />
 
-        <div className="all-users-list s-bar">
-          {sortedUsers.map((user, i) => {
-            return (
-              <div key={i} className="user-wrapper">
-                <p>{user.name}</p>
-                <p>{user.username}</p>
-              </div>
-            );
-          })}
-        </div>
+        {toggleUsers && (
+          <div className="users-wrapper">
+            <div className="all-users-list s-bar">
+              {users.filter(
+                (user) =>
+                  user._id !== loggedInUserId && // Exclude logged-in user
+                  !participants.some((p) => p.id === user._id) && // Exclude already added participants
+                  user.name
+                    .toLowerCase()
+                    .includes(currentParticipant.toLowerCase()) // Apply search filter
+              ).length === 0 ? (
+                <p className="no-users-found">No users found</p>
+              ) : (
+                users
+                  .filter(
+                    (user) =>
+                      user._id !== loggedInUserId && // Exclude logged-in user
+                      !participants.some((p) => p.id === user._id) && // Exclude already added participants
+                      user.name
+                        .toLowerCase()
+                        .includes(currentParticipant.toLowerCase()) // Apply search filter
+                  )
+                  .map((user, i) => {
+                    if (
+                      user._id === loggedInUserId ||
+                      participants.some((p) => p.id === user._id)
+                    )
+                      return;
+                    return (
+                      <div
+                        key={i}
+                        className="user-wrapper"
+                        onClick={() => {
+                          handleAddParticipant(user._id, user.username);
+                        }}
+                      >
+                        <p>{user.name}</p>
+                        <p>{user.username}</p>
+                      </div>
+                    );
+                  })
+              )}
+            </div>
+          </div>
+        )}
 
         {/* Show Added Participants */}
         {participants.length > 0 && (
           <div className="participants-list">
             {participants.map((p, index) => (
-              <span key={index} className="participant">
-                {p}
-              </span>
+              <div key={index} className="participant">
+                <span>{p.name}</span>
+                <RxCross2
+                  onClick={() => {
+                    handleRemoveParticipant(p.id);
+                  }}
+                ></RxCross2>
+              </div>
             ))}
           </div>
         )}
         <SubmitButton
-          label="Send"
+          label="Create Chat"
           isLoading={!groupName || participants.length === 0}
         />
       </form>
